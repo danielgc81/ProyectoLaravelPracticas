d004acdac6fa7e8275e6e0c35707e4f469983dd4
Daniel Garrido <danielgc81@educastur.es>
2026-03-02 13:06:46 +0100

    Implementación de middleware auth y guest para protección de rutas

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 
-// Rutas para las vistas que sirven los formularios de Registro e Iniciar Sesión
-Route::get('/login', function () {
-   return view('auth.login');
-})->name('login');
-Route::get('/register', function() {
-   return view('auth.register');
-})->name('register');
+// Rutas solo invitados (usuarios no autenticados)
+Route::middleware('guest')->group(function () {
+   // Rutas para las vistas que sirven los formularios de Registro e Iniciar Sesión
+   Route::get('/login', function() { return view('auth.login'); })->name('login');
+   Route::get('/register', function() { return view('auth.register'); })->name('register');
+   // Rutas encargadas del ciclo de autententicación de usuarios: nuevas cuentas e inicio de sesión
+   Route::post('/register-validated', [LoginController::class, 'register'])->name('register-validated');
+   Route::post('/log-in', [LoginController::class, 'login'])->name('log-in');
+});
 
-// ** Este inicio es una prueba **
-Route::get('/inicio', function () {
-   return view('index');
-})->name('inicio');
+// Rutas solo autenticados
+Route::middleware('auth')->group(function () {
+   // ** Este inicio es una prueba **
+   Route::get('/inicio', function() { return view('index'); })->name('inicio');
 
-// Rutas encargadas del ciclo de autententicación de usuarios: nuevas cuentas, inicio de sesión y cierre de sesión
-Route::post('/register-validated', [LoginController::class, 'register'])->name('register-validated');
-Route::post('/log-in', [LoginController::class, 'login'])->name('log-in');
-Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
-
+   // Ruta encargada de cierre de sesión
+   Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+});
