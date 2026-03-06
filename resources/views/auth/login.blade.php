9203de4cdf892e536f61e001f7c907cbea9e36b0
Daniel Garrido <danielgc81@educastur.es>
2026-03-06 16:19:23 +0100

    register estilado (no definitivo)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,12 +8,15 @@
 </head>
 <body>
    <main class=" bg-red-400 h-dvh flex justify-center items-center">
-      <form method="POST" action="{{ route('log-in') }}" class="bg-gray-200 flex flex-col p-3.5 w-96 rounded-2xl">
+      <form method="POST" action="{{ route('log-in') }}" class="bg-gray-100 flex flex-col p-3.5 w-96 rounded-2xl shadow-xl">
          @csrf
+         <h1 class="text-3xl font-bold self-center m-2.5">Log in</h1>
          @error('credentials')
-            <p>{{ $message }}</p>
+            <div class="flex items-center bg-red-300 gap-4 text-white border border-red-500 py-2 px-1.5 mb-2.5">
+               <img src="https://cdn-icons-png.flaticon.com/128/463/463612.png" alt="Error credenciales" class="max-w-8 h-8">
+               <p>{{ $message }}</p>
+            </div>
          @enderror
-         <h1 class="text-2xl font-bold self-center m-2.5">Log in</h1>
          <div class="flex flex-col mb-4">
             <label for="email" class="mb-1.5">Email</label>
             <input id="email" type="email" name="email" required class="bg-white py-1.5 px-3 rounded-xl focus:outline-none text-gray-600">
@@ -22,7 +25,7 @@
             <label for="password" class="mb-1.5">Contraseña</label>
             <input id="password" type="password" name="password" required class="bg-white py-1.5 px-3 rounded-xl focus:outline-none text-gray-600">
          </div>
-         <div class="flex items-center justify-between mb-6">
+         <div class="flex items-center justify-between mb-8">
             <div class="flex items-center gap-1.5">
                <input id="remember" type="checkbox" name="remember" class="w-4 h-4 accent-red-500">
                <label for="remember" class="text-xs">Mantener sesión</label>
@@ -31,7 +34,7 @@
                <p class="text-xs">¿No tienes cuenta? <a href="{{ route('register') }}" class="text-red-500 underline">Regístrate</a></p>
             </div>
          </div>
-         <button type="submit" class="bg-red-500 py-4 rounded-4xl text-white font-bold mb-4 cursor-pointer">Acceder</button>
+         <button type="submit" class="bg-red-500 outline-2 outline-red-500 py-4 rounded-4xl text-white font-bold mb-4 cursor-pointer">Acceder</button>
       </form>
    </main>
 </body>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,25 +7,33 @@
    @vite('resources/css/app.css')
 </head>
 <body>
-   <main class="flex">
-      <form method="POST" action="{{ route('register-validated') }}">
+   <main class="bg-red-400 h-dvh flex justify-center items-center">
+      <form method="POST" action="{{ route('register-validated') }}" class="bg-gray-100 flex flex-col p-3.5 w-96 rounded-2xl shadow-xl">
          @csrf
-         <div class="">
-            <label for="user">Nombre</label>
-            <input id="user" type="text" name="name" required>
+         <h1 class="text-3xl font-bold self-center m-2.5">Registrarse</h1>
+         <div class="flex flex-col mb-4">
+            <label for="user" class="mb-1.5">Nombre de Usuario</label>
+            <input id="user" type="text" name="name" required class="bg-white py-1.5 px-3 rounded-xl focus:outline-none text-gray-600">
+            @error('name')
+               <span class="text-red-500 text-xs mt-1">{{ $message }}</span>
+            @enderror
          </div>
-         <div class="">
-            <label for="email">Email</label>
-            <input id="email" type="email" name="email" required>
+         <div class="flex flex-col mb-6">
+            <label for="email" class="mb-1.5">Email</label>
+            <input id="email" type="email" name="email" required class="bg-white py-1.5 px-3 rounded-xl focus:outline-none text-gray-600">
+            @error('email')
+               <span class="text-red-500 text-xs mt-1">{{ $message }}</span>
+            @enderror
          </div>
-         <div class="">
-            <label for="password">Contraseña</label>
-            <input id="password" type="password" name="password" required>
+         <div class="flex flex-col mb-6">
+            <label for="password" class="mb-1.5">Contraseña</label>
+            <input id="password" type="password" name="password" required class="bg-white py-1.5 px-3 rounded-xl focus:outline-none text-gray-600">
+            @error('password')
+               <span class="text-red-500 text-xs mt-1">{{ $message }}</span>
+            @enderror
          </div>
-         <div class="">
-            <p>¿Ya tienes cuenta? <a href="{{ route('login') }}">Inicia sesión</a></p>
-         </div>
-         <button type="submit">Registrarse</button>
+         <button type="submit" class="bg-red-500 outline-2 outline-red-500 py-4 rounded-4xl text-white font-bold mb-4 cursor-pointer">Registrarse</button>
+         <a href="{{ route('login') }}" class="py-4 rounded-4xl text-red-500 outline-2 outline-red-500 font-bold mb-4 cursor-pointer text-center">Log in</a>
       </form>
    </main>
 </body>
