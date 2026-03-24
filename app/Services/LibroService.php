59638c194518ec0d161b87b3fe2d1811fa31b798
Daniel Garrido <danielgc81@educastur.es>
2026-03-24 11:58:46 +0100

    Mejora del método getAll en LibroService, se elimina codigo duplicado

--- app/Services/LibroService.php
+++ app/Services/LibroService.php
@@ -6,44 +6,22 @@ use App\Models\Libro;
 class LibroService {
 
    public function getAll (?string $search = null, string $orderBy = 'created_at', string $direction = 'desc', ?string $genre = null) {
-      if ($orderBy === 'valoraciones') {
-         // Cuando ordenamos por n de valoraciones,
-         // este metodo crea una tabla valoraciones_count en cada libro
-         // para ordenar por esa tabla
-         $query = Libro::withCount('valoraciones');
+      $query = $orderBy === 'valoraciones' ? Libro::withCount('valoraciones') : Libro::query();
 
-         // Lógica de la busqueda
-         if ($search) {
-               $query->where(function ($q) use ($search) {
-                  $q->where('title', 'like', "%{$search}%")
-                     ->orWhere('author', 'like', "%{$search}%");
-               });
-         }
-
-         if ($genre) {
-            $query->where('genre', $genre);
-         }
-
-         $query->orderBy('valoraciones_count', $direction);
-      } else {
-         $query = Libro::latest();
-
-         // Lógica de la busqueda
-         if ($search) {
-               $query->where(function ($q) use ($search) {
-                  $q->where('title', 'like', "%{$search}%")
-                     ->orWhere('author', 'like', "%{$search}%");
-               });
-         }
-
-         if ($genre) {
-            $query->where('genre', $genre);
-         }
-
-         // Este método elimina el orden de latest() y aplica
-         // el especificado en sus parámetros
-         $query->reorder($orderBy, $direction);
+      if ($search) {
+         $query->where(function ($q) use ($search) {
+            $q->where('title', 'like', "%{$search}%")
+               ->orWhere('author', 'like', "%{$search}%");
+         });
       }
+
+      if ($genre) {
+         $query->where('genre', $genre);
+      }
+
+      $query->orderBy(
+         $orderBy === 'valoraciones' ? 'valoraciones_count' : $orderBy, $direction
+      );
 
       return $query->get();
    }
